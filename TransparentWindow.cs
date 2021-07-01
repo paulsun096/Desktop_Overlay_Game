2ca3f04e804258da8af40f58a0dd311b12e21734
Paul Sun <paulsun096@gmail.com>
2021-06-30 23:59:11 -0500

    Add files via upload

---
+++ TransparentWindow.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+public class TransparentWindow : MonoBehaviour
+{
+    [DllImport("user32.dll")]
+    public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+
+    [DllImport("user32.dll")]
+    static extern int SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
+
+    [DllImport("user32.dll")]
+    static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+    private struct MARGINS
+    {
+        public int cxLeftWidth;
+        public int cdRightWidth;
+        public int cyTopHeight;
+        public int cyBottomHeight;
+    }
+
+    [DllImport("Dwmapi.dll")]
+    private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
+
+    const int GWL_EXSTYLE = -20;
+    const uint WS_EX_LAYERED = 0x00080000;
+    const uint WS_EX_TRANSPARENT = 0x00000020;
+
+    static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+
+    const uint LWA_COLORKEY = 0x00000001;
+
+    public void Start()
+#if !UNITY_EDITOR_
+    { 
+        //MessageBox(new IntPtr(0), "Hello World!", "Hello Dialog", 0);
+        IntPtr hWnd = GetActiveWindow();
+
+        MARGINS margins = new MARGINS { cxLeftWidth = -1};
+        DwmExtendFrameIntoClientArea(hWnd, ref margins);
+
+        //unlock the window to interact with the os
+        SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED);//| WS_EX_TRANSPARENT);
+        SetLayeredWindowAttributes(hWnd, 0, 0, LWA_COLORKEY);
+
+        //hWnd window handle
+        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, 0);
+#endif
+
+        Application.runInBackground = true;
+    }
+
+
+}
---
+++ smew.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smew : MonoBehaviour
+{
+
+
+    public Rigidbody rb;
+    public GameObject Target;
+    public float speed;
+    private Vector3 point;
+    private bool dragging = false;
+    private float distance;
+    public bool x = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        /*
+         if (x == true)
+        {
+            dragging = true;
+            
+        }
+        
+        else
+        {
+            dragging = false;   
+        }
+        x = !x;
+         
+         */
+        
+        dragging = true;
+
+      
+
+
+
+        //dragging = true;
+    }
+    void OnMouseUp()
+    {
+    
+        dragging = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }
+ 
+        //x = !x;
+        //Vector3 Dir = Target.transform.position - gameObject.transform.position;
+        //rb.AddForce(Dir.normalized * speed);
+    }
+}
